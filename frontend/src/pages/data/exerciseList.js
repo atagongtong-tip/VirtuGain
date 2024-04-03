2661a78d944cec159599beb18c4e30e22674b9ff
atagongtong-tip <qaatagongtong@tip.edu.ph>
2024-04-04 00:00:30 +0800

    front and back added

---
+++ frontend/src/pages/data/exerciseList.js
@@ -0,0 +1,63 @@
+export const EXERCISES_PER_TYPE = {
+  underWeight: ["squats", "deadlifts", "pushups", "curls"],
+  healthyWeight: ["lraise", "squats", "deadlifts", "pushups"],
+  overWeight: ["lunges", "squats", "deadlifts", "pushups"],
+  obese: ["jacks", "squats", "lunges", "deadlifts"],
+};
+
+export const EXERCISES = {
+  curls: {
+    name: "Bicep Curls",
+    imageFile: "bicep.jpg",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    link: "/curl",
+  },
+  deadlifts: {
+    name: "Deadlifts",
+    imageFile: "lift.jpg",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    link: "/lift",
+  },
+  jacks: {
+    name: "Jump and Jacks",
+    imageFile: "jacks.jpg",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    link: "/jack",
+  },
+  lraise: {
+    name: "Lateral Raise",
+    imageFile: "squat.jpg", // ! No image found
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    link: "/raise",
+  },
+  lunges: {
+    name: "Lunges",
+    imageFile: "lunges.jpg",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    link: "/lunge",
+  },
+  pushups: {
+    name: "Push Ups",
+    imageFile: "pushup.jpg",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    link: "/pushup",
+  },
+  squats: {
+    name: "Squats",
+    imageFile: "squat.jpg",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    link: "/squat",
+  },
+  barbellrow: {
+    name: "Barbell Row",
+    imageFile: "squat.jpg",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    link: "/barbellrow",
+  },
+  overheadpress: {
+    name: "Overhead press",
+    imageFile: "squat.jpg",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    link: "/overheadpress",
+  },
+};
